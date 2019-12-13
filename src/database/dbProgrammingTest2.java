98cea062eeb5f1014c8c7f13e88cc5a4e9260b19
Shynar <31266225+shynardev@users.noreply.github.com>
2019-12-13 08:59:00 -0800

    add changes to dbProgrammingTest2

--- src/database/dbProgrammingTest2.java
+++ src/database/dbProgrammingTest2.java
@@ -1,7 +1,7 @@
 package database;
 
 import java.sql.Connection;
-
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -23,20 +23,38 @@ public class dbProgrammingTest2 {
 		
 		Statement st = con.createStatement();
 
+//		Passing parameters through executeUpdate method 
 		String query = "select * from db1.books";
 
 		
-		PreparedStatement ps = con.prepareStatement("insert into db1.books values(?,?)"); 
+		PreparedStatement ps = con.prepareStatement("insert into db1.books values(?,?)", ResultSet.TYPE_SCROLL_INSENSITIVE); 
 		
 		ps.setInt(1, 3);
 		ps.setString(2, "Programming");
 		
-		ps.executeUpdate();
+//		ps.executeUpdate();
 		
 		ResultSet rs= st.executeQuery(query);
 		while (rs.next()){
 			System.out.println(rs.getInt(1)+" : "+rs.getString(2));
 		}
+		
+//		Fetching meta information of query results 
+		
+		DatabaseMetaData md = con.getMetaData();
+		System.out.println(md.getDatabaseProductName());
+		System.out.println(md.getDatabaseMajorVersion());
+		System.out.println(md.getDriverName());
+		System.out.println(md.getURL());
+		System.out.println(md.getUserName());
+		System.out.println(md.getCatalogs());
+		ResultSet columns = md.getColumns(null, null, "books", null);
+		while(columns.next()) {
+			System.out.print("Column name and size: "+ columns.getString("column_name"));
+			System.out.print("("+ columns.getInt("column_size")+") ");
+			System.out.println("Data type: " + columns.getString("type_name"));
+		}
+		
 		
 		
 		
